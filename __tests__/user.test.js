ec1b9d086f6dd5dcfd9a314c127115af8b222a4b
ariftoteles <arif.santoso4@bithealth.com>
2023-10-18 19:26:44 +0700

    add root

--- __tests__/user.test.js
+++ __tests__/user.test.js
@@ -1,6 +1,8 @@
 const request = require("supertest");
 const app = require("../app");
-const { User } = require("../models");
+const { User, Photo } = require("../models");
+
+const { generateToken } = require("../helpers/jwt")
 
 // test api register
 describe("POST /users/register", () => {
@@ -81,6 +83,9 @@ describe("POST /users/login", () => {
   it("Should response 200", (done) => {
     request(app)
       .post("/users/login")
+      .set({
+        access_token: "sdasdasds"
+      })
       .send({
         email: "admin@mail.com",
         password: "123456",
@@ -94,3 +99,66 @@ describe("POST /users/login", () => {
       });
   });
 });
+
+
+describe("Get all photos", () => {
+
+  let token
+  let id
+
+  beforeAll(async() => {
+    try {
+      const user = await User.create({
+        username: "asdas",
+        email: "dasdas",
+        password: "dsadasd"
+      });
+  
+      token = generateToken({
+        id: user.id,
+        email: user.email,
+        username: user.username
+      })
+
+      const photo = await Photo.create({
+        title: "dsdas"
+        caption: "dasdas",
+        image_url: "dasdasdas",
+        UserId: user.id
+      })
+  
+      id = photo.id
+
+    } catch (error) {
+      console.log(error);
+    }
+    }
+  )
+
+  it("Should response 200", (done) => {
+    request(app)
+    .get("/photos")
+    .set({
+      access_token: token
+    })
+    .expect(200)
+  })
+
+  it("Should response 200", (done) => {
+    request(app)
+    .get("/photos/" + id)
+    .set({
+      access_token: token
+    })
+    .expect(200)
+  })
+
+
+  afterAll(() => {
+    // destroy photo
+    // destroy user
+  })
+
+
+ 
+})
